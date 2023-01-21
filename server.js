2aba371da095816424b84fae4240465ed4783d8d
dhinadk28 <dhinadk28@gmail.com>
2023-01-21 11:12:21 +0530

    url change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:['https://astonishing-klepon-5ad827.netlify.app/','*'],
+    origin:['https://astonishing-klepon-5ad827.netlify.app','*'],
     methods:['GET','POST','PUT','DELETE'],
     
     credentials:true
