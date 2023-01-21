e167820c3f5fba38d95a0c987dcefbe8845db635
dhinadk28 <dhinadk28@gmail.com>
2023-01-21 11:08:02 +0530

    profile

--- models/userModel.js
+++ models/userModel.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://res.cloudinary.com/devatchannel/image/upload/v1602752402/avatar/avatar_cugq40.png"
+        default: "https://res.cloudinary.com/dbgp3j7j9/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1674279367/samples/people/profile_i5hex0.jpg"
     }
 }, {
     timestamps: true
